4642ccdcbfb3d6736d73f6eda93e5c1f4aefbf21
nicholasalvesx <nicholas.alves2007@gmail.com>
2025-04-10 16:40:15 -0300

    worker update

--- src/webapi/VCommerce.Api/Controllers/AuthController.cs
+++ src/webapi/VCommerce.Api/Controllers/AuthController.cs
@@ -16,17 +16,14 @@ public class AuthController : ControllerBase
 {
     private readonly ITokenService _tokenService;
     private readonly UserManager<ApplicationUser> _userManager;
-    private readonly RoleManager<IdentityRole> _roleManager;
     private readonly IConfiguration _configuration;
 
     public AuthController(ITokenService tokenService, 
         UserManager<ApplicationUser> userManager,
-        RoleManager<IdentityRole> roleManager, 
         IConfiguration configuration)
     {
         _tokenService = tokenService;
         _userManager = userManager;
-        _roleManager = roleManager;
         _configuration = configuration;
     }
 
@@ -173,4 +170,4 @@ public class AuthController : ControllerBase
         await _userManager.UpdateAsync(user);
         return NoContent();
     }
-}
+} 
